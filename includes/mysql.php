688abf63d59bccbe2734107751417d8374a305f3
Helge <dochelge@gmail.com>
2012-12-05 19:44:45 +1300

    I added a switch for the mysql data connection in order to work at home with different credentials.
    	modified:   includes/mysql.class.php
    	modified:   includes/mysql.php

--- includes/mysql.php
+++ includes/mysql.php
@@ -1,6 +1,12 @@
 <?php
 # connect to mysql
-$db_connect = mysql_connect('192.168.100.23:3306', 'admin', 'zt90undr');
+
+if($_SERVER['DOCUMENT_ROOT'] == "/Applications/XAMPP/xamppfiles/htdocs"){
+	$db_connect = mysql_connect('localhost', 'root', 'inkl67z');
+}
+else{
+	$db_connect = mysql_connect('192.168.100.23:3306', 'admin', 'zt90undr');
+}
 $testing=mysql_select_db('coural', $db_connect); 
 if (!$testing) die("error".mysql_error());
 ?>
