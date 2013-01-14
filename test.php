7864c2bb507145ee331c1c2cd326375484cebf9b
Helge Dzierzon <dochelge@gmail.com>
2013-01-14 22:01:02 +1300

    Worked on testing email via exchange

--- test.php
+++ test.php
@@ -3,6 +3,20 @@
 require_once('includes/mysql.php');
 require_once('includes/mysql_aid_functions.php');
 require_once('includes/mail_aid_functions.php');
-
+/*
+$fp = fsockopen("mail.coural.co.nz", 25, $errno, $errstr, 30);
+if (!$fp) {
+    echo "$errstr ($errno)<br />\n";
+} else {
+    $out = "GET / HTTP/1.1\r\n";
+    $out .= "Host: www.example.com\r\n";
+    $out .= "Connection: Close\r\n\r\n";
+    fwrite($fp, $out);
+    while (!feof($fp)) {
+        echo fgets($fp, 128);
+    }
+    fclose($fp);
+}
+*/
 send_test_mail();
 ?>
