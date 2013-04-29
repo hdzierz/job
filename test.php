8280ca9b66eff76bb1588dc76c408782f7254577
Helge Dzierzon <dochelge@gmail.com>
2013-04-29 22:55:08 +1200

    Bla

--- test.php
+++ test.php
@@ -4,8 +4,14 @@ require_once('includes/mysql.php');
 require_once('includes/mysql_aid_functions.php');
 require_once('includes/mail_aid_functions.php');
 
+$file = fopen("tickets_unredeemed.csv", "a");
 
-$qry = "SELECT * FROM parcel_job_ticket WHERE start IS NOT NULL AND end IS NOT NULL AND start <= end";
+ini_set('max_execution_time', 3*60*60);
+
+/*$qry = "SELECT * FROM parcel_job_ticket 
+LEFT JOIN parcel_job
+ON parcel_job.job_id = parcel_job_ticket.job_id
+WHERE start IS NOT NULL AND end IS NOT NULL AND start <= end AND order_date > '2012-01-01'";
 
 $res = query($qry);
 
@@ -15,7 +21,22 @@ while($item = mysql_fetch_object($res)){
 		query($qry);
 	}
 }
+*/
+$qry = "SELECT parcel_job.job_id,parcel_tickets.ticket_no, is_redeemed_D, order_date FROM parcel_tickets
+LEFT JOIN parcel_job
+ON parcel_job.job_id=parcel_tickets.job_id
+LEFT JOIN parcel_job_route 
+ON parcel_job_route.ticket_no = parcel_tickets.ticket_no
+WHERE parcel_job.order_date > '2012-03-31' AND parcel_job.order_date <= '2013-03-31'
+AND is_redeemed_D IS NULL AND is_redeemed_P IS NULL";
 
+$res = query($qry);
+$file = fopen("/tmp/tickets_unredeemed.csv", "a");
+while($item = mysql_fetch_array($res)){
+	fputcsv($file,$item);
+}
+
+fclose($file);
 
 //send_test_mail();
 ?>
