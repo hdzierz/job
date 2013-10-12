51800bd01fe080cfc5101c4dcab65d03c798d5b1
Helge <hdzierz@gmail.com>
2013-10-12 12:00:55 +0400

    Changed mysql conection so that the cloud conects to he local server wuth the correct credentials.

--- includes/mysql.class.php
+++ includes/mysql.class.php
@@ -291,6 +291,10 @@ if($_SERVER['DOCUMENT_ROOT'] == "/Applications/XAMPP/xamppfiles/htdocs"){
 	$MYSQL = new MySQL('localhost', 'coural');
 	$MYSQL->connect('root','inkl67z');
 }
+else if($_SERVER['DOCUMENT_ROOT'] == "/var/www/html"){
+	$MYSQL = new MySQL('localhost', 'coural');
+        $MYSQL->connect('root','inkl67z');
+}
 else{
 	$MYSQL = new MySQL('192.168.100.23:3306', 'coural');
 	$MYSQL->connect('admin','zt90undr');
--- includes/mysql.php
+++ includes/mysql.php
@@ -4,9 +4,13 @@
 if($_SERVER['DOCUMENT_ROOT'] == "/Applications/XAMPP/xamppfiles/htdocs"){
 	$db_connect = mysql_connect('localhost', 'root', 'inkl67z');
 }
+else if($_SERVER['DOCUMENT_ROOT'] == "/var/www/html"){
+	$db_connect = mysql_connect('localhost', 'root', 'inkl67z');
+}
 else{
 	$db_connect = mysql_connect('192.168.100.23:3306', 'admin', 'zt90undr');
 }
+
 $testing=mysql_select_db('coural', $db_connect); 
 if (!$testing) die("error".mysql_error());
 ?>
